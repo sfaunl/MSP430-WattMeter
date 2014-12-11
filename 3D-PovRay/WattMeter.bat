9257a0a1583907ac20b8d13bd10c32d201d1fc89
Sefa Unal <sefaunal@hotmail.com>
2014-12-11 11:53:21 +0200

    First version.

---
+++ 3D-PovRay/WattMeter.bat
@@ -0,0 +1,3 @@
+@echo off
+
+"pvengine.exe" +W320 +H240 +FN +L"c:/windows/fonts" +L"C:/Program Files (x86)/eagle3d/ulp//../povray" +O"C:/Users/Sefa/Desktop/wattmeter/Devre/V2/3D/WattMeter.png" /RENDER "C:/Users/Sefa/Desktop/wattmeter/Devre/V2/3D/WattMeter.pov" 
